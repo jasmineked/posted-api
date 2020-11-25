46bd9e0f9ad2946035b79b4de15ce58966841a9d
gundyn <nathan.gundy@gmail.com>
2020-11-25 13:51:03 -0600

    Fix: fileSchema
    Co-authored-by: Chris chrisfarish32@gmail.com
    Co-authored-by: J writetojehee@gmail.com
    Co-authored-by: jked630@icloud.com

--- app/models/file.js
+++ app/models/file.js
@@ -13,16 +13,9 @@ const fileSchema = new mongoose.Schema({
   tag: {
     type: String,
     required: true
-  },
-  dateModified: {
-    // time: {type: Date, default: Date.now}
-    type: Date
-  },
-  content: {
-    type: String,
-    required: true
+  }
 }, {
-   timestamps: true
+  timestamps: true
 })
 
 module.exports = mongoose.model('File', fileSchema)
