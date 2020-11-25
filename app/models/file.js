d09dde3c2fdb76b6157cd937cfbd20bb757b8b29
jasmineked <jked630@icloud.com>
2020-11-25 13:54:55 -0500

    feat: create file schema model
    Co-authored-by: Chris chrisfarish32@gmail.com
    Co-authored-by: J writetojehee@gmail.com
    Co-authored-by: Nathan nathan.gundy@gmail.com

---
+++ app/models/file.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const fileSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  fileName: {
+    type: String,
+    required: true
+  },
+  tag: {
+    type: String,
+    required: true
+  },
+  dateModified: {
+    // time: {type: Date, default: Date.now}
+    type: Date
+  },
+  content: {
+    type: String,
+    required: true
+}, {
+   timestamps: true
+})
+
+module.exports = mongoose.model('File', fileSchema)
