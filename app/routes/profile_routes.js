38729bbac53be67f258d9a1dd2fcde3c03b56a17
jasmineked <>
2020-12-15 09:40:10 -0500

    build: add delete profile route

--- app/routes/profile_routes.js
+++ app/routes/profile_routes.js
@@ -91,6 +91,22 @@ router.get('/profiles', requireToken, (req, res, next) => {
     // if an error occurs, pass it to the handler
     .catch(next)
 })
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
+router.delete('/profiles/:id', requireToken, (req, res, next) => {
+  Profile.findById(req.params.id)
+    .then(handle404)
+    .then(profile => {
+      // throw an error if current user doesn't own `example`
+      requireOwnership(req, profile)
+      // delete the example ONLY IF the above didn't throw
+      profile.deleteOne()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
 
 
 module.exports = router
